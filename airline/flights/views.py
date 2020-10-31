bdc62db5c2375a1c55d8e38eb52ee4fe0f6bac77
“firstPole” <vimalmcafifty@gmail.com>
2020-10-31 11:40:29 +0530

    added another function to detail out flight details by their id also created super user to manage flights and airport from admin portal

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -6,3 +6,8 @@ def index(request):
     return render(request,"flights/index.html",{
         "flights": Flight.objects.all()
     })
+def flight(request,flight_id):
+    flt = Flight.objects.get(id=flight_id)
+    return render(request,"flights/flight.html",{
+        "flight" : flt
+    })
